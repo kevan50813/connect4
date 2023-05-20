42bd0b15d4746e96f8afd60abc98c7827d0913bb
Kevan Jordan <kevanjordan9@gmail.com>
2023-05-20 14:13:53 +0100

    began baic HTML game

---
+++ connect4.js
@@ -0,0 +1,29 @@
+
+var board;
+var tiles = Array.from(board.querySelectorAll(".tile"));
+var currentPlayer = "red";
+var gameOver=false;
+
+var rows=6;
+var columns=7;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame(){
+    board= [];
+    for(let r=0;r<rows;r++){
+        let row=[];
+        for(let c=0;c<columns;c++){
+            row.push(' ');
+
+            let tile=document.createElement("div");
+            tile.id=r.toString()+"-"+c.toString();
+            tile.classList.add("tile");
+            document.getElementById("board").append("tile");
+        }
+        board.push(row);
+    }
+}
+
