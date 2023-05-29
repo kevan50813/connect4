07441a0fb1c25603380c263ab15eed580b44c515
Kevan Jordan <kevanjordan9@gmail.com>
2023-05-29 12:58:56 +0100

    basic connect 4 game

--- connect4.js
+++ connect4.js
@@ -1,29 +1,92 @@
+val_c1 = 1
+val_c2 = 1
+val_c3 = 1
+val_c4 = 1
+val_c5 = 1
+val_c6 = 1
+val_c7 = 1
+turn = 1
 
-var board;
-var tiles = Array.from(board.querySelectorAll(".tile"));
-var currentPlayer = "red";
-var gameOver=false;
 
-var rows=6;
-var columns=7;
+//checking the winner
 
-window.onload = function() {
-    setGame();
-}
+function check(player) {
+    setTimeout(() => {
 
-function setGame(){
-    board= [];
-    for(let r=0;r<rows;r++){
-        let row=[];
-        for(let c=0;c<columns;c++){
-            row.push(' ');
+        for (i = 1; i <= 7; i++) {
+            for (j = 1; j <= 3; j++) {
+                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 3}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()
+                }
 
-            let tile=document.createElement("div");
-            tile.id=r.toString()+"-"+c.toString();
-            tile.classList.add("tile");
-            document.getElementById("board").append("tile");
+            }
         }
-        board.push(row);
-    }
+
+        for (i = 1; i <= 6; i++) {
+            for (j = 1; j <= 4; j++) {
+                if (document.getElementById(`c${j}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 1}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 2}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 3}r${i}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()
+                }
+
+            }
+
+        }
+
+        for (i = 1; i <= 4; i++) {
+            for (j = 1; j <= 3; j++) {
+                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 1}r${j + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 2}r${j + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 3}r${j + 3}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()
+                }
+
+            }
+        }
+
+        for (i = 1; i <= 4; i++) {
+            for (j = 6; j >= 4; j--) {
+                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 1}r${j - 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 2}r${j - 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 3}r${j - 3}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()
+                }
+
+            }
+        }
+
+    }, 200)
+
 }
+
+
+
+//playing
+document.querySelectorAll(".column").forEach((e) => {
+    e.addEventListener("click", () => {
+
+        sum = eval(`val_${e.id}`)
+        eval(`val_${e.id}++`)
+
+
+        if (sum <= 6 && turn % 2 != 0) {
+            document.getElementById(`${e.id}r${sum}`).style.backgroundColor = "red"
+            turn++
+            check('red')
+            document.getElementById("whosturn").innerText = "Yellow's Turn"
+        }
+        
+        else if (sum <= 6 && turn % 2 == 0) {
+            document.getElementById(`${e.id}r${sum}`).style.backgroundColor = "yellow"
+            turn++
+            check('yellow')
+            document.getElementById("whosturn").innerText = "Red's Turn"
+
+        }
+       
+
+
+
+
+    })
+})
 
