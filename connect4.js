e793909bb82bce55b4aad0520e126fa8fd253297
Kevan Jordan <kevanjordan9@gmail.com>
2023-06-03 15:20:42 +0100

    added win text

--- connect4.js
+++ connect4.js
@@ -16,8 +16,8 @@ function check(player) {
         for (i = 1; i <= 7; i++) {
             for (j = 1; j <= 3; j++) {
                 if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 3}`).style.backgroundColor == `${player}`) {
-                    alert(`${player} wins`)
-                    location.reload()
+                    document.getElementById("winner").innerText = `${player} wins`
+                    return true;
                 }
 
             }
@@ -26,8 +26,8 @@ function check(player) {
         for (i = 1; i <= 6; i++) {
             for (j = 1; j <= 4; j++) {
                 if (document.getElementById(`c${j}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 1}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 2}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 3}r${i}`).style.backgroundColor == `${player}`) {
-                    alert(`${player} wins`)
-                    location.reload()
+                    document.getElementById("winner").innerText = `${player} wins`
+                    return true;
                 }
 
             }
@@ -37,8 +37,8 @@ function check(player) {
         for (i = 1; i <= 4; i++) {
             for (j = 1; j <= 3; j++) {
                 if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 1}r${j + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 2}r${j + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 3}r${j + 3}`).style.backgroundColor == `${player}`) {
-                    alert(`${player} wins`)
-                    location.reload()
+                    document.getElementById("winner").innerText = `${player} wins`
+                    return true;
                 }
 
             }
@@ -47,13 +47,13 @@ function check(player) {
         for (i = 1; i <= 4; i++) {
             for (j = 6; j >= 4; j--) {
                 if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 1}r${j - 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 2}r${j - 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 3}r${j - 3}`).style.backgroundColor == `${player}`) {
-                    alert(`${player} wins`)
-                    location.reload()
+                    document.getElementById("winner").innerText = `${player} wins`
+                    return true;
                 }
 
             }
         }
-
+        return false;
     }, 200)
 
 }
@@ -71,21 +71,24 @@ document.querySelectorAll(".column").forEach((e) => {
         if (sum <= 6 && turn % 2 != 0) {
             document.getElementById(`${e.id}r${sum}`).style.backgroundColor = "red"
             turn++
-            check('red')
-            document.getElementById("whosturn").innerText = "Yellow's Turn"
+            if(check('red')){
+                e.removeEventListener("click");
+            }
+            else{
+                document.getElementById("whosturn").innerText = "Yellow's Turn"
+            }
         }
         
         else if (sum <= 6 && turn % 2 == 0) {
             document.getElementById(`${e.id}r${sum}`).style.backgroundColor = "yellow"
             turn++
-            check('yellow')
-            document.getElementById("whosturn").innerText = "Red's Turn"
-
+            if(check('yellow')){
+                e.removeEventListener("click");
+            }
+            else{
+                document.getElementById("whosturn").innerText = "Red's Turn"
+            }
         }
-       
-
-
-
 
     })
 })
